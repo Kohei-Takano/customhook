fe066d8d9b3e2fe6641aef14ca0d3f7ca76b0022
Kohei-Takano <132127471+Kohei-Takano@users.noreply.github.com>
2023-05-20 22:00:01 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import{User}from"./types/api/user";
+import axios from "axios";
+import { User } from "./types/api/user";
 import { useState } from "react";
 import { UserProfile } from "./types/userProfile";
 
-const user = {
-  id: 1,
-  name: "じゃけぇ",
-  email: "1233@sas.com",
-  address: "adresssssss"
-};
-
 export default function App() {
-  const [userProfiles,setUserProfiles]=useState<Array<UserProfile>>([]);
-  const onClickFetchUser=()=>{
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res)=>{
-      const data=res.data.map((user)=>({
-        id:user.id,
-        
-
-      }))
-    })
-  }
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      });
+  };
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      <UserCard user={user} />
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
